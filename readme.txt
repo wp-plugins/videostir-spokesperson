55472cae2cbd3ed5e3634cc1535c2acab2bb8ec9
shy_frenkel <shy_frenkel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-10 20:54:37 +0000

    version 1.5.8 Added menu bar for customers testimonials
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/trunk@802160 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
 === VideoStir Spokesperson ===
 Contributors: alexey_bass, shy_frenkel
 Donate link: http://videostir.com/
-Tags: video, spokesperson, embed, player, flash, transparent, greenscreen, chroma, clip, background, conversion, marketing, ad, webcasterwp, change background
+Tags: video, spokesperson, embed, player, flash, transparent, greenscreen, chroma, clip, background, conversion, marketing, ad, change background
 Requires at least: 3.0
 Tested up to: 3.5.1
-Stable tag: 1.5.7
+Stable tag: 1.5.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -114,6 +114,9 @@ The [VideoStir Engine]( http://videostir.com/engine/?ref=wp) is an exclusive pat
 7. Simple Steps – upload,  tune,  enjoy.
 
 == Changelog ==
+= 1.5.8 =
+* Addition of new sub menu with customers testimonials
+
 = 1.5.7 =
 * Addition of new sub menu with coming features
 * Video Tour feature that guides your visitors around your website
--- videostir-spokesperson.php
+++ videostir-spokesperson.php
@@ -6,7 +6,7 @@ defined('ABSPATH') OR exit;
   Plugin Name: VideoStir Spokesperson
   Plugin URI: http://wordpress.org/extend/plugins/videostir-spokesperson/
   Description: With this plugin you can easily adjust and embed VideoStir clip into your website pages and posts.
-  Version: 1.5.7
+  Version: 1.5.8
   Author: VideoStir team
   Author URI: http://videostir.com/?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin
  */
@@ -32,7 +32,7 @@ class VideoStir
      * Plugin version.
      * @var sting
      */
-    const VERSION = '1.5.7';
+    const VERSION = '1.5.8';
     
     /**
      * WP option name where plugin's version is saved.
@@ -226,6 +226,7 @@ class VideoStir
         add_submenu_page('videostir_options', 'All Videos', 'All videos', 8, 'videostir_options', array(&$this, 'cf_all_video'));
         add_submenu_page('videostir_options', 'Add New Video', 'Add new', 8, 'videostir_options_sub', array(&$this, 'cf_actions'));
         add_submenu_page('videostir_options', 'Coming features', 'Coming features', 8, 'videostir_coming_features', array(&$this, 'cf_coming_features'));
+		add_submenu_page('videostir_options', 'Testimonials', 'Testimonials', 8, 'videostir_customers_testimonials', array(&$this, 'cf_testimonials'));
 
 	}
 	
@@ -254,6 +255,11 @@ class VideoStir
     function cf_coming_features()
     {
 	   include 'page-coming-features.php';
+	}
+	
+	function cf_testimonials()
+    {
+	   include 'page-testimonials.php';
 	}
 	
 }
