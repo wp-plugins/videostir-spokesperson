adae932e349e28377241da0becb738e5f2f2e90f
shy_frenkel <shy_frenkel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-10 20:53:05 +0000

    version 1.5.8 Added menu bar for customers testimonials
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/tags/1.5.8@802159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ page-coming-features.php
@@ -0,0 +1,48 @@
+<?php
+global $wpdb;
+?>
+<?php include 'css-script.php'; ?>
+
+<div class="wrap">
+	<h2><img class="logo" src="<?php echo $this->logo; ?>" alt="VideoStir" /> Coming features -  <a href="#" style="font-size:18px" onclick="UserVoice.showPopupWidget(); return false;">Let us know what you think</a> </h2>
+	<div style="width: 3%;float: left;">&nbsp;</div>
+ 	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default;"><b>"Video Tour" around the internet - </b><a target="_blank" href="http://videostir.com/follow-me-demo.html?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin"> Live demo</a></h2>
+			<a href="http://videostir.com/follow-me-demo.html?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin" onclick="window.open(this.href); return false;" ><img src="/wp-content/plugins/videostir-spokesperson/img/online-presntation-tour.jpg" alt="VideoStir" /></a>
+			<div>&nbsp;</div>
+			<h3 style="cursor: default;">In a nutshell</h3>
+			<h4 style="margin-bottom: 2px;">1. Your floating clip runs in the foreground overlay</h4>
+			<p style="margin-top: 0px;">Same basic DIY floating clip that guides your viewers.</p>
+			<h4 style="margin-bottom: 2px;">2. The page in the background keeps changing from page to page while the video plays</h4>
+			<p style="margin-top: 0px;">You can choose to which pages to "jump" and when to jump there.</p>
+			<h4 style="margin-bottom: 2px;">3. Once clip is done a chosen page appears.</h4>
+			<p style="margin-top: 0px;">You can take your viewers to a video guided tour around the internet or around your website pages.</p>
+			<h3>Here is a live example of Shy Frenkel VideoStir's CEO explaining the coming feature: <a target="_blank" href="http://videostir.com/follow-me-demo.html?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin">http://videostir.com/follow-me-demo.html</a></h3>
+		</div>
+	</div>
+	<div style="width: 3%;float: left;">&nbsp;</div>
+ 	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default;"><b>Video On Image - already in Beta- </b><a target="_blank" href="http://videostir.com/video-on-image/?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin"> start here</a></h2>
+			<a href="http://videostir.com/video-on-image/?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin" onclick="window.open(this.href); return false;" ><img src="/wp-content/plugins/videostir-spokesperson/img/simple3x3-voi-tmp.jpg" alt="VideoStir" /></a>
+			<div>&nbsp;</div>
+			<h3 style="cursor: default;">In a nutshell</h3>
+			<h4 style="margin-bottom: 2px;">1. Upload Video + Upload Image => get a Video on Image mix</h4>
+			<p style="margin-top: 0px;">Same basic DIY floating clip with any image automatically mixes them both</p>
+			<h4 style="margin-bottom: 2px;">2. Download the result clip to your PC</h4>
+			<p style="margin-top: 0px;">Entirly automatically and takes less than 5 minutes - entirely free for now.</p>
+<a href="#" style="font-size:18px" onclick="UserVoice.showPopupWidget(); return false;">Let us know what you think</a>
+		</div>
+	</div>
+</div>
+ 
+<!-- uservoice feedback code with additional parameter video-hash-->
+<script type="text/javascript">
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/7Vrlg3EzRUNNpScfBFqTBg.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
+
