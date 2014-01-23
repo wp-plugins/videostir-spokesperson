9871f8ad2b3bdacf0efd560b0e8895f8e49a49df
shy_frenkel <shy_frenkel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-23 17:14:06 +0000

    version 1.5.101 Youtube clip id optional beta feature - appears when watching page from iphone/ipad
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/tags/1.5.11@843936 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ page-testimonials.php
@@ -0,0 +1,113 @@
+<?php
+global $wpdb;
+?>
+<?php include 'css-script.php'; ?>
+
+<div class="wrap">
+	<h2><img class="logo" src="<?php echo $this->logo; ?>" alt="VideoStir" /> Customers testimonials -  <a href="#" style="font-size:18px" onclick="UserVoice.showPopupWidget(); return false;">Let us know what you think</a> </h2>
+	<div style="width: 3%;float: left;">&nbsp;</div>
+ 	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default; height:350px; margin-left:15px;"><b>Mike Deluca &mdash; Real Estate &mdash; </b><a target="_blank" href="http://buycondosouthbeach.com/" onclick="window.open(this.href); return false;"> Link to website</a></h2>
+			<a style="float:left; margin-top:-310px;margin-left:15px;" href="http://buycondosouthbeach.com/" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/mike-deluca.jpg" alt="VideoStir" width="400"/></a>
+			<div>&nbsp;</div>
+			<h3 class="testimonial" style="margin-top:-295px;">&laquo; Imagine delivering your message personally 
+                to any potential clients visiting your website. Now, you have the
+                ability to turn them into kinetic clients by delivering a short 
+                personal message. Allowing people and clients to know you - 
+                before physically knowing you.&laquo; </h3>
+			</div>
+	</div>
+	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default; height:350px; margin-left:15px;"><b>Sean Smallwood &mdash; Family Law Attorney &mdash; </b><a target="_blank" href="http://affordablefamilylawyer.com" onclick="window.open(this.href); return false;"> Link to website</a></h2>
+			<a style="float:left; margin-top:-270px;margin-left:15px;" href="http://affordablefamilylawyer.com" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/testimonial-sean-smallwood.jpg" alt="VideoStir" width="400" /></a>
+			<div>&nbsp;</div>
+			<h3 class="testimonial" style="margin-top:-275px;">&laquo;My VideoStir clip offers my business website,
+                a look and feel that my competitors pay thousands of dollars to 
+                have with other companies. It also allows prospective clients to
+                get a feel for who I am in a way that a photo or a Youtube video 
+                just doesn't allow.&raquo; </h3>
+			</div>
+	</div>
+	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default; height:370px; margin-left:15px;"><b>Michael &mdash; Maui, HI &mdash;</b><a target="_blank" href="http://www.prosperyourmind.com/" onclick="window.open(this.href); return false;"> Link to website</a></h2>
+			<a style="float:left; margin-top:-330px;margin-left:15px;" href="http://www.prosperyourmind.com/" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/prosperyourmind.jpg" alt="VideoStir" width="400" /></a>
+			<div>&nbsp;</div>
+			<h3 class="testimonial" style="margin-top:-320px;">&laquo;I have been extremely pleased with VideoStir. 
+                <strong>I'm already seeing results from my main website and Facebook 
+                    business pages.</strong> Also the service and quick responses has 
+                    been outstanding even when my video needed to be corrected because 
+                    of my issues. I would highly recommend this service to anyone wanting 
+                    to promote their product or service.&raquo;</h3>
+			</div>
+	</div>
+	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default; height:350px; margin-left:15px;"><b>Glen &mdash; CEO The Life Coaching College &mdash;</b><a target="_blank" href="http://www.thelifecoachingcollege.com.au/" onclick="window.open(this.href); return false;"> Link to website</a></h2>
+			<a style="float:left; margin-top:-300px;margin-left:15px;" href="http://www.thelifecoachingcollege.com.au/" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/glen.jpg" alt="VideoStir" width="400" /></a>
+			<div>&nbsp;</div>
+			<h3 class="testimonial" style="margin-top:-330px;">&laquo;I was looking for a way to layer video over 
+                the top of my website and introduce my character to my posse. I 
+                sourced a few options but they were very expensive and technically 
+                challenging and then I discovered VideoStir. <strong>This program was quick, 
+                affordable and I had a result within an hour!</strong> I am really happy with 
+                the program and I recommend it to everyone of my students now. Thanks Guys!&raquo;</h3>
+			</div>
+	</div>
+	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default; height:420px; margin-left:15px;"><b>Daniel W Hawk &mdash; Sports Journalist &mdash; </b><a target="_blank" href="http://danielwhawk.com/" onclick="window.open(this.href); return false;"> Link to website</a></h2>
+			<a style="float:left; margin-top:-310px;margin-left:15px;" href="http://danielwhawk.com/" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/testimonial-daniel-w-hawk.jpg" alt="VideoStir" width="400"/></a>
+			<div>&nbsp;</div>
+			<h3 class="testimonial" style="margin-top:-390px;">&laquo;Building a website on your own is tough enough
+                but trying to get people to notice your site is another story in all
+                itself. <strong>With the help of VideoStir my web traffic has nearly
+                    doubled.</strong> VideoStir takes the time personally to help make
+                    your site a success. It is the best way to separate your self from
+                    the competition and to get your name out in the cyber world. 
+                    I would recommend VideoStir to anybody who is just starting their 
+                    website or have been on the internet for quite a while.&raquo; </h3>
+			</div>
+	</div>
+	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default; height:500px; margin-left:15px;"><b>K.A.Butler &mdash; Director GoDirect2Barristers &mdash;</b><a target="_blank" href="http://www.GoDirect2Barristers.co.uk" onclick="window.open(this.href); return false;"> Link to website</a></h2>
+			<a style="float:left; margin-top:-400px;margin-left:15px;" href="http://www.GoDirect2Barristers.co.uk" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/godirect2barristers.jpg" alt="VideoStir" width="400" /></a>
+			<div>&nbsp;</div>
+			<h3 class="testimonial" style="margin-top:-470px;">&laquo;In launching our new Directory & Barrister Locator service to the UK public 
+                we saw an opportunity to use a "walk on video spokesperson" as a guide to explain the site navigation to first time visitors. 
+                It has had an immediate impact and also the use of video and geo-mapping differentiates our site from competitors. 
+                We researched the many video spokesperson suppliers and chose VideoStir and were pleasantly impressed by their service and support. <strong>They went "the extra mile" to 
+                help us to ensure that our video displayed in the best quality possible.</strong>&raquo;</h3>
+			</div>
+	</div>
+	<div style="width: 3%;float: left;">&nbsp;</div>
+ 	
+
+</div>
+ <style>
+ .testimonial {
+	 font-size: 13pt;
+	 margin-top:-295px;
+	 margin-right:10px;
+	 max-width:350px;
+	 max-height:700px;
+	 float:right;
+	 font-family: Georgia, serif;
+	 color: #666;    
+	 border-radius: 5px;     
+	 font-style: italic; 
+	 line-height:150%;
+ }
+ </style>
+<!-- uservoice feedback code with additional parameter video-hash-->
+<script type="text/javascript">
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/7Vrlg3EzRUNNpScfBFqTBg.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
+
