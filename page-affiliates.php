bf0567fec9a6cca53d90c42e744f7e74658f6157
shy_frenkel <shy_frenkel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 14:02:09 +0000

    version 1.5.15 Optional Youtube clip id - change
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/tags/1.5.15@916579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ page-affiliates.php
@@ -0,0 +1,54 @@
+<?php
+global $wpdb;
+?>
+<?php include 'css-script.php'; ?>
+
+<div class="wrap">
+	<h2><img class="logo" src="<?php echo $this->logo; ?>" alt="VideoStir" /> Affiliate program </h2>
+	<div style="width: 3%;float: left;">&nbsp;</div>
+ 	<div id="formdiv" class="postbox " >
+		<div class="inside">
+			<h2 style="cursor: default;"><b>Join our affiliate program - earn money right away. </b><a href="#" style="font-size:18px" onclick="UserVoice.showPopupWidget(); return false;">(Contact us)</a></h2>
+			<div>&nbsp;</div>
+			<a href="http://videostir.com/affiliates/join-program?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin" onclick="window.open(this.href); return false;" ><img src="<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/728x90.gif"  alt="VideoStir" /></a>
+			<div>&nbsp;</div>
+			<h3 style="cursor: default;">In a nutshell</h3>
+			<h4 style="margin-bottom: 2px;">1. Register as an affiliate</h4>
+			<p style="margin-top: 0px;">Let us know who you are and we will activate your affiliate account. You will get your own affiliate link and additional info.</p>
+			<h4 style="margin-bottom: 2px;">2. Tell the world about us</h4>
+			<p style="margin-top: 0px;">Use your affiliate link on your website / blog / facebook / linkedin / emails etc. Tell your friends to use your links where they see fit.</p>
+			<h4 style="margin-bottom: 2px;">3. Collect your commission and monitor your leads.</h4>
+			<p style="margin-top: 0px;">Monitor your leads online. See how many registered / uploaded / paid for their clip at any second. Watch your commission grow and collect your payments (simple Paypal transfer from us to you).</p>
+			<h3>You can tell us a bit about yourself using this <a href="#" style="font-size:18px" onclick="UserVoice.showPopupWidget(); return false;"> link</a> or start the process on our website with this <a target="_blank" style="font-size:18px" href="http://videostir.com/affiliates/join-program?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin"> link </a> </h3>
+		</div>
+	</div>
+	
+	<div>
+		<div class="pricing-row"><strong>Simple process</strong> &mdash; No fuss. Register, get a link, collect payments</div>
+		<div class="pricing-row"><strong>In house monitoring page</strong> &mdash; A live page where you can see your leads and what they do. </div>
+		<div class="pricing-row"><strong>Marketing material</strong> &mdash; You can use VideoStir service for marketing our product. We will help you out with content and clips.</div>
+		<div class="pricing-row"><strong>Support</strong> &mdash; Our support team will be there to help you with any question you have.</div> 
+   </div>
+   
+ 	
+</div>
+ <style>
+ .pricing-row {
+    background: transparent url('<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/pricing-row.png') 0 bottom no-repeat;
+    width: 520px;
+    height: 41px;
+    padding: 15px 10px 10px 70px;
+    color: #89b13e;
+}
+</style>
+ 
+ 
+<!-- uservoice feedback code with additional parameter video-hash-->
+<script type="text/javascript">
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/7Vrlg3EzRUNNpScfBFqTBg.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
+
