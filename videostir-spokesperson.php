6e159c375552db60459918c423a460404d0649e9
alexey_bass <alexey_bass@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-14 17:21:24 +0000

    version 1.6.11 When clip end options
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/trunk@1044475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- videostir-spokesperson.php
+++ videostir-spokesperson.php
@@ -6,7 +6,7 @@ defined('ABSPATH') OR exit;
   Plugin Name: VideoStir Spokesperson
   Plugin URI: http://wordpress.org/extend/plugins/videostir-spokesperson/
   Description: With this plugin you can easily adjust and embed VideoStir clip into your website pages and posts.
-  Version: 1.6.10
+  Version: 1.6.11
   Author: VideoStir team
   Author URI: http://videostir.com/?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin
  */
