8f88762a49859df66bb178c5cace198beddb55df
alexey_bass <alexey_bass@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-30 18:31:55 +0000

    version 1.6.13 All videos page improvement
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/tags/1.6.13@1057029 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rigth-bar.php
@@ -0,0 +1,80 @@
+<style>
+.presentation_btn{
+	background:transparent url(<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/presentation_btn.png) 0 0 no-repeat;
+    display: block;
+    height: 56px;
+    margin: 6px 0 0 150px;
+    position: relative;
+    width: 192px;
+	float:left;
+}
+.presentation_btn:hover{
+	background:transparent url(<?php bloginfo('url'); ?>/wp-content/plugins/videostir-spokesperson/img/presentation_btn.png) 0 -60px no-repeat;
+}
+.presentation_btn img{
+    position: absolute;
+    right: -28px;
+    top: -3px;
+}
+.presentation_btns span{
+    color: #444444;
+    display: block;
+    float: left;
+    font-size: 9pt;
+    margin: 26px 5px 0 4px;
+}
+</style>
+
+
+
+
+<div id="formdiv" class="postbox " >
+    <div class="inside">
+		<a href="#" onclick="window.open('http://videostir.com/go/step-register?utm=wp-start-here')" class="presentation_btn"></a>
+        <!--<h3 style="cursor: default;">Demo</h3>-->
+		
+        <iframe title="YouTube video player" class="youtube-player" type="text/html" width="100%" height="300" src="http://www.youtube.com/embed/hzy6lmnAezk?theme=light&color=white&showinfo=0&controls=1&wmode=transparent&rel=0" frameborder="0" allowFullScreen></iframe>
+		
+        <div>&nbsp;</div>
+
+        <h3 style="cursor: default;">In a nutshell</h3>
+
+        <h4 style="margin-bottom: 2px;">1. You shoot a video.</h4>
+        <p style="margin-top: 0px;">Just shoot a short video based on <a href="http://videostir.com/guidelines?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin">our guidelines</a> (one-colored wall etc.)</p>
+
+        <h4 style="margin-bottom: 2px;">2. You quickly follow VideoStir's 3 steps - Upload, Tune, Enjoy.</h4>
+        <p style="margin-top: 0px;">Within a few minutes our VideoStir Engine will create a background-less floating clip</p>
+
+        <h4 style="margin-bottom: 2px;">3. You use your floating clip.</h4>
+        <p style="margin-top: 0px;">Embed it into your webpage of choice. It's easy to do.</p>
+
+        <p>Visit us at <a target="_blank" href="http://videostir.com/?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin">http://videostir.com</a></p>
+    </div>
+</div>
+
+
+
+<div id="formdiv" class="postbox " >
+    <div class="inside">
+        <h3 style="cursor: default;">Filming guidelines</h3>
+        <iframe title="YouTube video player" class="youtube-player" type="text/html" width="100%" height="300" src="http://www.youtube.com/embed/GoVyfxMpHhk?theme=light&color=white&showinfo=0&controls=1&wmode=transparent&rel=0" frameborder="0" allowFullScreen></iframe>
+    </div>
+</div>
+
+<div id="formdiv" class="postbox " >
+    <div class="inside">
+        <h3 style="cursor: default;">Our customers floating clips</h3>
+        <iframe title="YouTube video player" class="youtube-player" type="text/html" width="100%" height="300" src="http://www.youtube.com/embed/BT5yeguI7so?theme=light&color=white&showinfo=0&controls=1&wmode=transparent&rel=0" frameborder="0" allowFullScreen></iframe>
+    </div>
+</div>
+
+
+
+<!-- uservoice feedback code with additional parameter video-hash-->
+<script type="text/javascript">
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/7Vrlg3EzRUNNpScfBFqTBg.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
