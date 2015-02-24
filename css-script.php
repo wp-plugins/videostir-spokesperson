1a72a80e790ada4a3d31c24f01b4a68e25edf75f
alexey_bass <alexey_bass@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-24 14:54:23 +0000

    version 1.7.1  embed one line bug fixing
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/trunk@1098172 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- css-script.php
+++ css-script.php
@@ -142,13 +142,14 @@
 			return false;
 		}
 		
-		if (code.indexOf('VS.Player.Show')===-1)
+		if (code.indexOf('VS.Player.show')===-1)
 		{
 			alert(
                 'Wait, you should first quickly prepare you VideoStir floating clip.'
                 + '\n\nPaste the 3 lines you got from videostir.com after transforming your video into a floating clip in the text box below.'
                 + '\nClick "Next" to adjust the parameters that will appear and choose the pages/posts that will hold the clip from the list.'
             );
+            return false;
 		}
         return true;
     }
