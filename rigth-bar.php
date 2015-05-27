013cfe3910917a59e12de088cde6b4b8c9e7df43
alexey_bass <alexey_bass@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-27 16:42:30 +0000

    version 1.8.2 small design changes
    git-svn-id: https://plugins.svn.wordpress.org/videostir-spokesperson/tags/1.8.2@1168707 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rigth-bar.php
@@ -0,0 +1,52 @@
+
+<div id="formdiv" class="postbox " style=" box-shadow: 0 5px 15px rgba(0,0,0,0.15);border: 1px solid rgba(0,0,0,0.25);" >
+    <div class="inside">
+
+
+        <iframe src="https://videostir.com/demo/test-on-outside?page=wp" width="370" height="110" style="padding:0px;margin:0px;" align="center"></iframe>
+        <iframe src="https://videostir.com/demo/start-on-outside/?page=wp" width="370" height="100" style="padding:5px;" align="center"></iframe>
+        <iframe title="YouTube video player" style="padding-top:10px" class="youtube-player" type="text/html" width="100%" height="300" src="http://www.youtube.com/embed/BjL-8vIp9-s?theme=light&color=white&showinfo=0&controls=1&wmode=transparent&rel=0" frameborder="0" allowFullScreen></iframe>
+		
+        <div>&nbsp;</div>
+
+        <h3 style="cursor: default;">In a nutshell</h3>
+
+        <h4 style="margin-bottom: 2px;">1. You shoot a video.</h4>
+        <p style="margin-top: 0px;">Just shoot a short video based on <a href="http://videostir.com/guidelines?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin">our guidelines</a> (one-colored wall etc.)</p>
+
+        <h4 style="margin-bottom: 2px;">2. You quickly follow VideoStir's 3 steps - Upload, Tune, Enjoy.</h4>
+        <p style="margin-top: 0px;">Within a few minutes our VideoStir Engine will create a background-less floating clip</p>
+
+        <h4 style="margin-bottom: 2px;">3. You use your floating clip.</h4>
+        <p style="margin-top: 0px;">Embed it into your webpage of choice. It's easy to do.</p>
+
+        <p>Visit us at <a target="_blank" href="http://videostir.com/?utm_source=wp-plugin&utm_medium=plugin&utm_campaign=wp-plugin">http://videostir.com</a></p>
+    </div>
+</div>
+
+
+
+<div id="formdiv" class="postbox " style=" box-shadow: 0 5px 15px rgba(0,0,0,0.15);border: 1px solid rgba(0,0,0,0.25);" >
+    <div class="inside">
+        <h3 style="cursor: default;">Filming guidelines</h3>
+        <iframe title="YouTube video player" class="youtube-player" type="text/html" width="100%" height="300" src="http://www.youtube.com/embed/GoVyfxMpHhk?theme=light&color=white&showinfo=0&controls=1&wmode=transparent&rel=0" frameborder="0" allowFullScreen></iframe>
+    </div>
+</div>
+
+<div id="formdiv" class="postbox " style=" box-shadow: 0 5px 15px rgba(0,0,0,0.15);border: 1px solid rgba(0,0,0,0.25);">
+    <div class="inside">
+        <h3 style="cursor: default;">Our customers floating clips</h3>
+        <iframe title="YouTube video player" class="youtube-player" type="text/html" width="100%" height="300" src="http://www.youtube.com/embed/BT5yeguI7so?theme=light&color=white&showinfo=0&controls=1&wmode=transparent&rel=0" frameborder="0" allowFullScreen></iframe>
+    </div>
+</div>
+
+
+
+<!-- uservoice feedback code with additional parameter video-hash-->
+<script type="text/javascript">
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/7Vrlg3EzRUNNpScfBFqTBg.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
+</script>
